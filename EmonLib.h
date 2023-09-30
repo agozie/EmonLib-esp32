b7e5ea2e11ad7eef4902bf7ed8c8a4ca4f3d0f4a
agozie <agozie@gmail.com>
2023-09-30 20:11:04 +0100

    Customized for Esp32-c3

--- EmonLib.h
+++ EmonLib.h
@@ -33,72 +33,67 @@
 // include the following line in main sketch inside setup() function:
 //  analogReadResolution(ADC_BITS);
 // otherwise will default to 10 bits, as in regular Arduino-based boards.
-#if defined(__arm__)
-#define ADC_BITS    12
-#else
-#define ADC_BITS    10
-#endif
+// #if defined(__arm__)
+// #define ADC_BITS    12
+// #else
+// #define ADC_BITS    10
+// #endif
 
-#if defined(ESP32)
-#define ADC_BITS    12
-#endif
+// #if defined(ESP32)
+#define ADC_BITS 12
+// #endif
 
-#define ADC_COUNTS  (1<<ADC_BITS)
-
+#define ADC_COUNTS (1 << ADC_BITS)
 
 class EnergyMonitor
 {
-  public:
+public:
+  void voltage(unsigned int _inPinV, double _VCAL, double _PHASECAL);
+  void current(unsigned int _inPinI, double _ICAL);
 
-    void voltage(unsigned int _inPinV, double _VCAL, double _PHASECAL);
-    void current(unsigned int _inPinI, double _ICAL);
+  void voltageTX(double _VCAL, double _PHASECAL);
+  void currentTX(unsigned int _channel, double _ICAL);
 
-    void voltageTX(double _VCAL, double _PHASECAL);
-    void currentTX(unsigned int _channel, double _ICAL);
+  void calcVI(unsigned int crossings, unsigned int timeout);
+  double calcIrms(unsigned int NUMBER_OF_SAMPLES);
+  void serialprint();
 
-    void calcVI(unsigned int crossings, unsigned int timeout);
-    double calcIrms(unsigned int NUMBER_OF_SAMPLES);
-    void serialprint();
-
-    long readVcc();
-    //Useful value variables
-    double realPower,
+  long readVcc();
+  // Useful value variables
+  double realPower,
       apparentPower,
       powerFactor,
       Vrms,
       Irms;
 
-  private:
+private:
+  // Set Voltage and current input pins
+  unsigned int inPinV;
+  unsigned int inPinI;
+  // Calibration coefficients
+  // These need to be set in order to obtain accurate results
+  double VCAL;
+  double ICAL;
+  double PHASECAL;
 
-    //Set Voltage and current input pins
-    unsigned int inPinV;
-    unsigned int inPinI;
-    //Calibration coefficients
-    //These need to be set in order to obtain accurate results
-    double VCAL;
-    double ICAL;
-    double PHASECAL;
+  //--------------------------------------------------------------------------------------
+  // Variable declaration for emon_calc procedure
+  //--------------------------------------------------------------------------------------
+  int sampleV; // sample_ holds the raw analog read value
+  int sampleI;
 
-    //--------------------------------------------------------------------------------------
-    // Variable declaration for emon_calc procedure
-    //--------------------------------------------------------------------------------------
-    int sampleV;                        //sample_ holds the raw analog read value
-    int sampleI;
+  double lastFilteredV, filteredV; // Filtered_ is the raw analog value minus the DC offset
+  double filteredI;
+  double offsetV; // Low-pass filter output
+  double offsetI; // Low-pass filter output
 
-    double lastFilteredV,filteredV;          //Filtered_ is the raw analog value minus the DC offset
-    double filteredI;
-    double offsetV;                          //Low-pass filter output
-    double offsetI;                          //Low-pass filter output
+  double phaseShiftedV; // Holds the calibrated phase shifted voltage.
 
-    double phaseShiftedV;                             //Holds the calibrated phase shifted voltage.
+  double sqV, sumV, sqI, sumI, instP, sumP; // sq = squared, sum = Sum, inst = instantaneous
 
-    double sqV,sumV,sqI,sumI,instP,sumP;              //sq = squared, sum = Sum, inst = instantaneous
+  int startV; // Instantaneous voltage at start of sample window.
 
-    int startV;                                       //Instantaneous voltage at start of sample window.
-
-    boolean lastVCross, checkVCross;                  //Used to measure number of times threshold is crossed.
-
-
+  boolean lastVCross, checkVCross; // Used to measure number of times threshold is crossed.
 };
 
 #endif
